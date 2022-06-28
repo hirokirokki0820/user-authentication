0875b8a199c1d29ab10dfed1e7d1300ff48969b1
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-28 17:12:41 +0900

    Add: Userモデルの作成, バリデーション追加,  Userテストコードの作成)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  # email　オブジェクトが保存される時点で小文字に変換する
+  before_save { email.downcase! }
+  # before_save { self.email = email.downcase }
+
+  # name のバリデーション
+  validates :name, presence: true, length: { maximum: 25 }
+
+  # email のバリデーション
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
