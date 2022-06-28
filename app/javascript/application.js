012f0d5a80c69592ebcad4541272cefd2be6c9ef
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-28 23:57:02 +0900

    Add: ユーザー登録フォームのデザイン(Semaitic UI)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,9 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "jquery"
+import "jquery-validation-rails"
 
+/* jquery 関数定義 */
 $(document).on("turbo:load", function() {
   /* ドロップダウン */
   $('.ui.dropdown').dropdown();
@@ -14,4 +16,56 @@ $(document).on("turbo:load", function() {
   /* アコーディオン */
   $('.ui.accordion').accordion();
 
-});
+  /* User入力フォームのバリデーション */
+//   $('.ui.form')
+//   .form({
+//     fields: {
+//       'user[name]'    : 'empty',
+//       'user[email]'   : 'empty',
+//       'user[password]' : ['minLength[6]', 'empty'],
+//       'user[password_confirmation]' : ['minLength[6]', 'empty']
+//     }
+//   })
+// ;
+
+// $('.ui.form')
+//   .form({
+//     fields: {
+//       name: {
+//         identifier: 'user[name]',
+//         rules: [
+//           {
+//             type   : 'empty',
+//             prompt : 'ユーザー名を入力してください'
+//           }
+//         ]
+//       },
+//       email: {
+//         identifier: 'user[email]',
+//         rules: [
+//           {
+//             type   : 'empty',
+//             prompt : 'メールアドレスを入力してください'
+//           }
+//         ]
+//       },
+//       password: {
+//         identifier: 'user[password]',
+//         rules: [
+//           {
+//             type   : 'empty',
+//             prompt : 'パスワードを入力してください'
+//           },
+//           {
+//             type   : 'minLength[6]',
+//             prompt : 'パスワードは6文字以上で入力してください'
+//           }
+//         ]
+//       }
+//     }
+//   })
+// ;
+
+
+
+}); //最後尾
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js"
+pin "jquery-validation-rails", to: "https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"
