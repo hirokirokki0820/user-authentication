9eb165008e6f6995d4edb7646d0a1bca0daf835d
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-28 21:21:29 +0900

    Add: Semantic UI のセットアップ

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+// Semantic UI
+// = require semantic-ui
+
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
+
+$(document).on("turbo:load", function() {
+  /* ドロップダウン */
+  $('.ui.dropdown').dropdown();
+
+  /* アコーディオン */
+  $('.ui.accordion').accordion();
+
+});
