2ba1e96c034f00d47b7fe9f831ca62108d9ccf7c
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-29 20:08:34 +0900

    Fix: 'field_with_errors'によるフォームのレイアウト崩れを修正(config/aplication.rb に設定を追加)

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module UserAuthentication
     config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # inputタグに 「field_with_errors」 を自動挿入しないようにする
+    # エラー時のレイアウト崩れを防ぐため
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
