2818588b1dd395a5a0447abbdf694fa014c9f84f
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-30 16:37:32 +0900

    Add: ユーザー登録時にユーザーが作成されることを確認するテスト

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,9 +2,8 @@ require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   # 新規ユーザー登録用の統合テスト
-  # ユーザー登録ボタンを押したときに（ユーザー情報が無効であるために）ユーザーが作成されないことを確認する
+  # ユーザー登録時に（ユーザー情報が無効であるために）ユーザーが作成されないことを確認する
   test "invalid signup information" do
-    # 新規ユーザー登録フォームに入る
     get new_user_path
     assert_no_difference 'User.count' do
       # users_pathに対してポストリクエストを送信する
@@ -18,6 +17,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template "users/new"
     # エラー発生時に error クラスが追加されているかどうか
     assert_select 'div.field.error'
+  end
+
+  # ユーザー登録時にユーザーが作成されることを確認する
+  test "valid signup information" do
+    get new_user_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user:
+                    { name: 'test user',
+                    email: 'test@example.com',
+                    password: 'password',
+                    password_confirmation: 'password' } }
+    end
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_template "users/show"
+    # showページの指定タグ内にユーザー名が表示されているかを確認
+    assert_select "p.header", "test user"
+    assert_select "div.ui.card>.content>.header", "test user"
+    assert_select "div.ui.message.success"
   end
 
 end
