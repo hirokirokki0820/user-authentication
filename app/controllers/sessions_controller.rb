0d2de412647b1460767b711497f42cce35cf6bc0
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-07-02 16:01:13 +0900

    Add: ログイン機構の追加, ログインテストの追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to user
+    else
+      flash.now[:error] = "ログインに失敗しました"
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path, status: :see_other
+  end
 end
